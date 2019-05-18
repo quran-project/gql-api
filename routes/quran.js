9be09fcf50a6f0f72199884e61912d66a70389b5
kingrayhan <rayhan095@gmail.com>
2019-05-19 03:02:57 +0600

    remove express from project

--- routes/quran.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router()
-const surahController = require('./../controllers/surahController')
-
-Router.get('/', surahController.getAllSurah)
-
-Router.get('/:sura', surahController.getSurah)
-Router.get('/:sura/:ayah', surahController.getAyah)
-
-module.exports = Router
